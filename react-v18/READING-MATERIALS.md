29a3c6b4f08b442fa98adb7ea953de36070a202b
= <=>
2022-08-18 15:15:21 +0400

    added reactDomClient

--- react-v18/READING-MATERIALS.md
+++ react-v18/READING-MATERIALS.md
@@ -1,3 +1,4 @@
 
 ###  useTransition  `https://reactjs.org/docs/hooks-reference.html#usetransition` 
+###  reactDomclient `https://reactjs.org/docs/react-dom-client.html`
 
---
+++ react-v18/src/reactDOMClient/index.js
@@ -0,0 +1,9 @@
+import {createRoot} from 'react-dom/client'; // import createRoot
+//import App from './App';
+
+const container = document.getElementById("root");
+const root = createRoot(container); //create reactRoot
+root.render(<App/>)
+
+
+// reactv18 you should be write this
